db7037eea27dcafee153f918d94b5056a35b0f9d
Gondocs Robert <gondocs.robert@gmail.com>
2023-09-19 23:36:02 +0200

    added animations for other navbar elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; 
 import DropdownMenu from './dropDown';
+import '../css/Navbar.css';
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
@@ -23,7 +24,7 @@ const Navbar = () => {
     <nav className="bg-gray-800 p-4">
       <div className="mx-2 flex items-center">
     
-        <Link to="/" className="text-white hover:text-gray-400 hidden md:block mr-24 ml-4 text-[1.3rem]">
+        <Link to="/" className="text-white hover:text-gray-400 hidden md:block mr-24 ml-4 text-[1.3rem] hover-scale hover-scale:hover">
           Főoldal
         </Link>
 
@@ -39,7 +40,7 @@ const Navbar = () => {
         </div>
 
         {/* Left Side - Összehasonlítás */}
-        <Link to="/osszehasonlitas" className="text-white hover:text-gray-400 hidden md:block mr-8">
+        <Link to="/osszehasonlitas" className="text-white hover:text-gray-400 hidden md:block mr-8 hover-scale hover-scale:hover">
           Összehasonlítás
         </Link>
 
@@ -49,17 +50,17 @@ const Navbar = () => {
             <input
               type="text"
               placeholder="Keresés"
-              className="pl-8 pr-10 py-2 rounded-lg bg-gray-700 text-white focus:outline-none w-full"
+              className="pl-8 pr-10 py-2 rounded-lg bg-gray-700 text-white focus:outline-none w-full hover-scale hover-scale:hover"
             />
           </div>
         </div>
 
         {/* Right Side - Login and Register */}
-        <Link to="/belepes" className="text-white hover:text-gray-400 ml-80" >
+        <Link to="/belepes" className="text-white hover:text-gray-400 ml-80 hover-scale hover-scale:hover" >
           Belépés
         </Link>
         <h5 className='text-white'>/</h5>
-        <Link to="/regisztracio" className="text-white hover:text-gray-400 ml-0">
+        <Link to="/regisztracio" className="text-white hover:text-gray-400 ml-0 hover-scale hover-scale:hover">
           Regisztráció
         </Link>
       </div>
