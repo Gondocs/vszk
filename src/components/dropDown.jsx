048672f66c6c9e6bf611b974671041db58a0dbdd
Gondocs Robert <gondocs.robert@gmail.com>
2023-09-19 12:43:21 +0200

    style updated for dropdowns

--- src/components/dropDown.jsx
+++ src/components/dropDown.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const DropdownMenu = () => {
   return (
-    <div className="absolute bg-gray-800 z-10 w-48 mt-2 py-2 space-y-2 border border-gray-700">
-      <ul>
-        <li>Menu 1</li>
-        <li>Menu 2</li>
-        <li>Menu 3</li>
-        <li>Menu 4</li>
-        <li>Menu 5</li>
-        <li>Menu 6</li>
-        <li>Menu 7</li>
+    <div className="absolute bg-gray-800 z-10 w-48 mt-0 py-2 space-y-2 border border-gray-700 text-white focus:outline-none">
+      <ul className="mt-2 mb-4 ml-2 mr-2">
+        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 1</li>
+        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 2</li>
+        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 3</li>
+        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 4</li>
+        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 5</li>
+        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 6</li>
+        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 7</li>
       </ul>
     </div>
   );
