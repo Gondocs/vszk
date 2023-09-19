1d658b1d7f6de412c471faba89eab352b7623e14
Gondocs Robert <gondocs.robert@gmail.com>
2023-09-19 16:24:47 +0200

    changed width of menu dropdown

--- src/components/dropDown.jsx
+++ src/components/dropDown.jsx
@@ -15,15 +15,14 @@ const menuItems = [
 
 const DropdownMenu = () => {
   return (
-    <div className="absolute bg-gray-800 z-10 w-48 mt-0 py-2 space-y-2 border border-gray-700 text-white focus:outline-none">
+    <div className="absolute bg-gray-800 z-10 w-64 mt-0 py-2 space-y-2 border border-gray-700 text-white focus:outline-none">
       <ul className="mt-2 mb-4 ml-2 mr-2">
         {menuItems.map((item) => (
     <Link to={item.link}>
-      <button className="block pl-6 pr-24 py-4 text-white hover:bg-gray-600">
+      <button className="block pl-6 pr-40 py-4 text-white hover:bg-gray-600">
         <li className="">{item.label}</li>
       </button>
     </Link>
-
         ))}
       </ul>
     </div>
