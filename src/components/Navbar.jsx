6877d3bf6935fe40d0e905d3f1f9a47c6cd023b2
Gondocs Robert <gondocs.robert@gmail.com>
2023-09-19 12:35:31 +0200

    Added new button in navbar with a correctly working hover function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,41 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom'; 
+import DropdownMenu from './dropDown';
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
   };
 
+  const handleMouseEnter = () => {
+    setDropdownVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownVisible(false);
+  };
+
   return (
     <nav className="bg-gray-800 p-4">
-      <div className=" mx-2 flex items-center">
+      <div className="mx-2 flex items-center">
     
         <Link to="/" className="text-white hover:text-gray-400 hidden md:block mr-24 ml-4 text-[1.3rem]">
           Főoldal
         </Link>
 
         {/* Left Side - Szoftverek Dropdown */}
-        <div className="relative group hidden md:block mr-16 ml-4">
-          <button
-            className="text-white focus:outline-none"
-            onMouseOver={() => toggleDropdown()} 
-            onMouseOut={() => {/*toggleDropdown()}*/}} // TODO: need to add keeping dropdown open when hovering over the other options!!! 
-          >
-            Szoftverek
-          </button>
-          {showDropdown && (
-            <div className="absolute bg-gray-800 z-10 w-48 mt-2 py-2 space-y-2 border border-gray-700">
-              {/* Add your dropdown options here */}
-              <a href="#/" className="block px-4 py-2 text-white hover:bg-gray-600">
-                Option 1
-              </a>
-              <a href="#/" className="block px-4 py-2 text-white hover:bg-gray-600">
-                Option 2
-              </a>
-              {/* Add more options */}
-            </div>
-          )}
+
+        <div
+          className="relative group hidden md:block mr-16 ml-4"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <button className='text-white focus:outline-none'>Szoftverek</button>
+          {isDropdownVisible && <DropdownMenu />}
         </div>
 
         {/* Left Side - Összehasonlítás */}
---
+++ src/components/dropDown.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const DropdownMenu = () => {
+  return (
+    <div className="absolute bg-gray-800 z-10 w-48 mt-2 py-2 space-y-2 border border-gray-700">
+      <ul>
+        <li>Menu 1</li>
+        <li>Menu 2</li>
+        <li>Menu 3</li>
+        <li>Menu 4</li>
+        <li>Menu 5</li>
+        <li>Menu 6</li>
+        <li>Menu 7</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownMenu;
