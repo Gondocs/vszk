cc2993ed76dc7975245ed8b0c869285ed1f2ac6c
Gondocs Robert <gondocs.robert@gmail.com>
2023-09-19 12:49:25 +0200

    changed dropdown button's layout

--- src/components/dropDown.jsx
+++ src/components/dropDown.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 const DropdownMenu = () => {
-  return (
+  return ( // TODO: need to add links to the menu items
+           // TODO: need to add animation for dropdown menu
     <div className="absolute bg-gray-800 z-10 w-48 mt-0 py-2 space-y-2 border border-gray-700 text-white focus:outline-none">
       <ul className="mt-2 mb-4 ml-2 mr-2">
-        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 1</li>
-        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 2</li>
-        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 3</li>
-        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 4</li>
-        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 5</li>
-        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 6</li>
-        <li className="block px-4 py-2 text-white hover:bg-gray-600">Menu 7</li>
+        <li className="block px-4 py-4 text-white hover:bg-gray-600">Menu 1</li>
+        <li className="block px-4 py-4 text-white hover:bg-gray-600">Menu 2</li>
+        <li className="block px-4 py-4 text-white hover:bg-gray-600">Menu 3</li>
+        <li className="block px-4 py-4 text-white hover:bg-gray-600">Menu 4</li>
+        <li className="block px-4 py-4 text-white hover:bg-gray-600">Menu 5</li>
+        <li className="block px-4 py-4 text-white hover:bg-gray-600">Menu 6</li>
+        <li className="block px-4 py-4 text-white hover:bg-gray-600">Menu 7</li>
       </ul>
     </div>
   );
