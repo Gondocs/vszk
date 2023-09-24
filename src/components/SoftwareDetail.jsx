7dcda6cfde349cc877ba3435a77601b6fbab3cbd
Gondocs Robert <gondocs.robert@gmail.com>
2023-09-24 19:22:25 +0200

    massively changed animation for all

---
+++ src/components/SoftwareDetail.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function SoftwareDetail({ softwareData }) {
+  const { name } = useParams();
+
+  // Find the software with the matching name
+  const selectedSoftware = softwareData.find(
+    (software) =>
+      software.name.replace(/\s+/g, '-').toLowerCase() === name
+  );
+
+  if (!selectedSoftware) {
+    return <div>Software not found</div>;
+  }
+
+  return (
+    <div>
+      <h2>{selectedSoftware.name}</h2>
+      <p>Company: {selectedSoftware.company}</p>
+      <p>Rating: {selectedSoftware.rating}</p>
+      <p>Description: {selectedSoftware.description}</p>
+    </div>
+  );
+}
+
+export default SoftwareDetail;
