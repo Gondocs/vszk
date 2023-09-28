000e97964f7d2076aaf90acf45da0018b7e25268
Gondocs Robert <gondocs.robert@gmail.com>
2023-09-29 01:51:04 +0200

    added transliteration and integrated all category data into the dropdown

--- src/components/Navbar/dropDown.jsx
+++ src/components/Navbar/dropDown.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import '../../css/dropDown.css';
 import { get } from '../api/api';
+import { transliterate } from '../api/transliteration';
+
 
 const DropdownMenu = () => {
   const [MainCategoryData, setMainCategoryData] = useState([]);
+  const [hoveredCategory, setHoveredCategory] = useState(null);
 
   useEffect(() => {
     get.Category().then((data) => setMainCategoryData(data));
@@ -13,18 +16,41 @@ const DropdownMenu = () => {
   const uniqueCategories = Array.from(
     new Set(MainCategoryData.map((category) => category.categoryGroup.name))
   );
- 
+
   console.log(MainCategoryData);
 
   return (
     <div className="absolute bg-gray-800 z-10 w-max mt-0 py-2 border border-gray-700 text-white focus:outline-none FadeInSmall">
-      <ul className="mt-2 mb-2 ml-4 mr-4">
+      <ul className="mt-2 mb-2">
         {uniqueCategories.map((categoryName) => (
-          <Link to={categoryName} key={categoryName}>
+          <li
+            key={categoryName}
+            className="relative group"
+            onMouseEnter={() => setHoveredCategory(categoryName)}
+            onMouseLeave={() => setHoveredCategory(null)}
+          >
+            <Link to={`/szoftverek/${transliterate(categoryName)}`}>
             <button className="w-full block text-left pl-6 pr-6 py-4 text-white hover:bg-gray-600">
-              <li className="">{categoryName}</li>
+              {categoryName}
             </button>
-          </Link>
+            </Link>
+
+            <ul
+              className={`py-2 sub-menu absolute top-0 left-full bg-gray-800 ${
+                hoveredCategory === categoryName ? 'FadeInSmall' : 'invisible opacity-0'
+              }`}
+            >
+              {MainCategoryData
+                .filter((category) => category.categoryGroup.name === categoryName)
+                .map((subcategory) => (
+                  <Link to={`/szoftverek/${transliterate(subcategory.name)}`} key={subcategory.name}>
+                    <button className="w-full block text-left pl-6 pr-6 py-4 text-white hover:bg-gray-600 hover:opacity-100">
+                      {subcategory.name}
+                    </button>
+                  </Link>
+                ))}
+            </ul>
+          </li>
         ))}
       </ul>
     </div>
---
+++ src/components/api/transliteration.js
@@ -0,0 +1,21 @@
+export function transliterate(text) {
+    const transliterationMap = {
+      'á': 'a',
+      'é': 'e',
+      'í': 'i',
+      'ó': 'o',
+      'ö': 'o',
+      'ő': 'o',
+      'ú': 'u',
+      'ü': 'u',
+      'ű': 'u',
+    };
+  
+    return text
+      .toLowerCase()
+      .split('')
+      .map((char) => transliterationMap[char] || char)
+      .join('')
+      .replace(/\s+/g, '-');
+  }
+  
