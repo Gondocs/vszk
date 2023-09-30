8df5f3ec7aad01aa85ddd168328bce4af9d180ba
Gondocs Robert <gondocs.robert@gmail.com>
2023-09-30 18:21:33 +0200

    enhanced softwareDetail page

--- src/components/SoftwareDetail.jsx
+++ src/components/SoftwareDetail.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import NotFound from './PageNotFound';
 import { get } from './api/api';
 import { showToast } from './toasts/toast';
+import StarIcon from '@mui/icons-material/Star';
 
 function SoftwareDetail() {
   const { name } = useParams();
@@ -29,28 +30,25 @@ function SoftwareDetail() {
 
 
   return (
-    <div className="p-16 mt-8">
-      <div className="bg-gray-200 p-16 rounded-lg shadow-lg flex items-center">
+    <div className="p-16">
+      <div className="bg-gray-200 p-12 rounded-lg shadow-lg flex items-center">
         <img
           src={SoftwareData.logo_link}
           alt="Software Logo"
-          className=" w-96 h-64 rounded-40 mr-6"
+          className=" rounded-40 mr-6"
           draggable="false"
         />
-        <div>
-          <h2 className="text-3xl font-semibold">{SoftwareData.name}</h2>
-          <p className="text-lg font-semibold">Rating: {SoftwareData.rating}</p>
-          <p className="text-lg mt-4">{SoftwareData.description}</p>
-          <div className="mt-6">
-            <a
-              href={SoftwareData.website}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="px-4 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-md text-gray-900 font-semibold transition duration-300 inline-block"
-            >
-              Tovább a szoftver oldalára
-            </a>
-          </div>
+        <div className='p-4'>
+          <h2 className="text-3xl font-semibold p-4">Név: {SoftwareData.name}</h2>
+          <p className="text-lg font-semibold pt-4 pb-4 pl-4 pr-0">Értékelés: 4,6 ideiglenes{SoftwareData.rating}
+                <StarIcon fontSize="medium" className='starmargin' style={{ color: 'rgb(255, 210, 48)' }}/>
+          </p>
+          <p className="text-lg mt-4 p-4">Leírás: {SoftwareData.description}</p>
+          <button className='ml-4 mt-6 px-4 py-2 bg-yellow-400 hover:bg-yellow-500 rounded-md text-gray-900 font-semibold transition duration-300 inline-block' 
+          onClick={() => window.open(SoftwareData.company.website)}>
+          Tovább a szoftver oldalára
+          </button> 
+
         </div>
       </div>
     </div>
