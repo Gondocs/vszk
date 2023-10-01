0992a84f540e473f30181ddbbdfc93af0b38d606
Gondocs Robert <gondocs.robert@gmail.com>
2023-10-01 22:19:08 +0200

    fixed the problem, where the pictures wouldn't resize correctly based on window size

--- src/components/CompanyList.jsx
+++ src/components/CompanyList.jsx
@@ -48,42 +48,45 @@ export const CompanyList = () => {
       <div className="w-3/4 p-4 bg-gray-200 rounded-40 ">
         <h1 className="text-2xl font-semibold mb-8 mt-2 ml-12 hover-scale-element:hover hover-scale-element">Céglista</h1>
         <ul>
-  {filteredCompanies.map((company) => (
-    <li key={company.softwareID} className="pb-8 px-4 hover-scale-element:hover hover-scale-element FadeInSmall">
-      <div className="bg-white rounded-25 p-16 border border-gray-400">
-        <div className="flex">
-
-          <Link to={`/szoftverek/${transliterate(company.name)}`}
-          className="flex items-center">
-            <img
-              src={company.logo_link}
-              alt="Software Placeholder"
-              className="w-64 h-32 ml-4 mr-20"
-              draggable="false"
-            />
-          </Link>
-          <div className='w-2/3'>
-            <Link
-              to={`/szoftverek/${transliterate(company.name)}`}
-              className="text-3xl font-semibold text-black"
-            >
-              {company.name}
-            </Link>
-            <Link to={`/szoftverek/${transliterate(company.name)}`}>
-            <p className="text-gray-600 text-xl mb-2 mt-2">{company.name}</p>
-            </Link>
-            <div className="flex items-center">
-              <span className="text-black text-lg mr-2">
-                Vélemények: 
-                <StarIcon fontSize="medium" className='starmargin' style={{ color: 'rgb(255, 210, 48)' }}/>
-              </span>
-            </div>
-            <p className="text-black">Leírás: {company.description}</p>
-          </div>
-        </div>
+        {filteredCompanies.map((company) => (
+  <li key={company.softwareID} className="pb-8 px-4 hover-scale-element:hover hover-scale-element FadeInSmall">
+    <div className="bg-white rounded-25 pr-12 pb-12 pt-12 border border-gray-400 flex">
+      {/* Container for the image (1/3 of the width) */}
+      <div className="w-1/3 flex justify-center items-center">
+        <Link to={`/szoftverek/${transliterate(company.name)}`} className="flex items-center">
+          <img
+            src={company.logo_link}
+            alt="Software Placeholder"
+            className="pl-10 pr-10"
+            draggable="false"
+            style={{ width: 'auto', height: 'auto' }}
+          />
+        </Link>
       </div>
-    </li>
-  ))}
+
+      {/* Container for the data (2/3 of the width) */}
+      <div className='w-2/3'>
+        <Link
+          to={`/szoftverek/${transliterate(company.name)}`}
+          className="text-3xl font-semibold text-black"
+        >
+          {company.name}
+        </Link>
+        <Link to={`/szoftverek/${transliterate(company.name)}`}>
+          <p className="text-gray-600 text-xl mb-2 mt-2">{company.name}</p>
+        </Link>
+        <div className="flex items-center">
+          <span className="text-black text-lg mr-2">
+            Vélemények:
+            <StarIcon fontSize="medium" className='starmargin' style={{ color: 'rgb(255, 210, 48)' }}/>
+          </span>
+        </div>
+        <p className="text-black">Leírás: {company.description}</p>
+      </div>
+    </div>
+  </li>
+))}
+
 </ul>
 
 
--- src/components/SoftwareList.jsx
+++ src/components/SoftwareList.jsx
@@ -85,9 +85,10 @@ const SoftwareList = () => {
 
   return (
     <div className="flex min-h-screen bg-gray-200 py-8 px-16 FadeInSmall">
-
       <div className="w-1/5 bg-gray-100 p-8 rounded-40 mr-16">
-        <h2 className="text-lg font-semibold mb-4 hover-scale-element:hover hover-scale-element">Szoftverkeresés</h2>
+        <h2 className="text-lg font-semibold mb-4 hover-scale-element:hover hover-scale-element">
+          Szoftverkeresés
+        </h2>
         <input
           type="text"
           placeholder="Szoftver neve..."
@@ -97,49 +98,71 @@ const SoftwareList = () => {
         />
       </div>
 
-      <div className="w-3/4 p-4 bg-gray-200 rounded-40 ">
-        <h1 className="text-2xl text-black font-semibold mb-8 mt-2 ml-12 hover-scale-element:hover hover-scale-element">Szoftverlista</h1>
+      <div className="w-3/4 p-4 bg-gray-200 rounded-40">
+        <h1 className="text-2xl text-black font-semibold mb-8 mt-2 ml-12 hover-scale-element:hover hover-scale-element">
+          Szoftverlista
+        </h1>
         <ul>
-  {filteredSoftwareData.map((software) => (
-    <li key={software.softwareID} className="pb-8 px-4 hover-scale-element:hover hover-scale-element FadeInSmall ">
-      <div className="bg-white rounded-25 p-16 border border-gray-400">
-        <div className="flex">
-
-        <Link to={`/szoftverek/${transliterate(software.category.categoryGroup.name)}/${transliterate(software.category.name)}/${transliterate(software.name)}`}
-        className="flex items-center">
-            <img
-              src={software.logo_link}
-              alt="Software Placeholder"
-              className="w-64 h-32 ml-4 mr-20"
-              draggable="false"
-            />
-        </Link>
-          <div className='w-2/3'>
-            <Link
-              to={`/szoftverek/${transliterate(software.category.categoryGroup.name)}/${transliterate(software.category.name)}/${transliterate(software.name)}`}
-              className="text-3xl font-semibold text-black"
+          {filteredSoftwareData.map((software) => (
+            <li
+              key={software.softwareID}
+              className="pb-8 px-4 hover-scale-element:hover hover-scale-element FadeInSmall"
             >
-              {software.name}
-            </Link>
-            <Link to={`/cegek/${transliterate(software.company.name)}`}>
-            <p className="text-gray-600 text-xl mb-2 mt-2">{software.company.name}</p>
-            </Link>
-            <div className="flex items-center">
-              <span className="text-black text-lg mr-2">
-                Vélemények: 
-                <StarIcon fontSize="medium" className='starmargin' style={{ color: 'rgb(255, 210, 48)' }}/>
-              </span>
-            </div>
-            <p className="text-gray-700">{software.description}</p>
-          </div>
-        </div>
-      </div>
-    </li>
-  ))}
-</ul>
-      {noResultsMessage}
+              <div className="bg-white rounded-25 pt-12 pb-12 pr-12 border border-gray-400 flex">
+                {/* Container for the image (1/3 of the width) */}
+                <div className="w-1/3 flex justify-center items-center">
+                  <Link
+                    to={`/szoftverek/${transliterate(
+                      software.category.categoryGroup.name
+                    )}/${transliterate(software.category.name)}/${transliterate(
+                      software.name
+                    )}`}
+                    className="flex items-center"
+                  >
+                    <img
+                      src={software.logo_link}
+                      alt="Software Placeholder"
+                      className="pl-10 pr-10"
+                      draggable="false"
+                      style={{ width: 'auto', height: 'auto' }}
+                    />
+                  </Link>
+                </div>
 
-
+                {/* Container for the data (2/3 of the width) */}
+                <div className="w-2/3">
+                  <Link
+                    to={`/szoftverek/${transliterate(
+                      software.category.categoryGroup.name
+                    )}/${transliterate(software.category.name)}/${transliterate(
+                      software.name
+                    )}`}
+                    className="text-3xl font-semibold text-black"
+                  >
+                    {software.name}
+                  </Link>
+                  <Link to={`/cegek/${transliterate(software.company.name)}`}>
+                    <p className="text-gray-600 text-xl mb-2 mt-2">
+                      {software.company.name}
+                    </p>
+                  </Link>
+                  <div className="flex items-center">
+                    <span className="text-black text-lg mr-2">
+                      Vélemények:
+                      <StarIcon
+                        fontSize="medium"
+                        className="starmargin"
+                        style={{ color: 'rgb(255, 210, 48)' }}
+                      />
+                    </span>
+                  </div>
+                  <p className="text-gray-700">{software.description}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        {noResultsMessage}
       </div>
     </div>
   );
