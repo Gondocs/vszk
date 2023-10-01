cb8df562ba5445a2fb297337aeb2dd19548ae0f2
Gondocs Robert <gondocs.robert@gmail.com>
2023-10-01 21:25:54 +0200

    added function to remove ',' and '.' from company names in the url in transliteration

--- src/components/api/transliteration.js
+++ src/components/api/transliteration.js
@@ -1,22 +1,23 @@
 export function transliterate(text) {
     
-    const transliterationMap = {
-      'á': 'a',
-      'é': 'e',
-      'í': 'i',
-      'ó': 'o',
-      'ö': 'o',
-      'ő': 'o',
-      'ú': 'u',
-      'ü': 'u',
-      'ű': 'u',
-    };
-  
-    return text
-      .toLowerCase()
-      .split('')
-      .map((char) => transliterationMap[char] || char)
-      .join('')
-      .replace(/\s+/g, '-');
-  }
-  
+  const transliterationMap = {
+    'á': 'a',
+    'é': 'e',
+    'í': 'i',
+    'ó': 'o',
+    'ö': 'o',
+    'ő': 'o',
+    'ú': 'u',
+    'ü': 'u',
+    'ű': 'u',
+  };
+
+  text = text.replace(/[,.]/g, '');
+
+  return text
+    .toLowerCase()
+    .split('')
+    .map((char) => transliterationMap[char] || char)
+    .join('')
+    .replace(/\s+/g, '-');
+}
