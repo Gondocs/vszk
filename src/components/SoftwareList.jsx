5a0c2afab7810c3eaf2e253663c364db86aa42c4
Gondocs Robert <gondocs.robert@gmail.com>
2023-10-02 02:07:27 +0200

    added loading for softwareList

--- src/components/SoftwareList.jsx
+++ src/components/SoftwareList.jsx
@@ -5,6 +5,9 @@ import { Link, useParams } from "react-router-dom";
 import { transliterate } from "./api/transliteration";
 import { get } from "./api/api";
 import { showToast } from "./toasts/toast";
+import { ClipLoader } from "react-spinners";
+import { css } from "@emotion/react";
+import { HashLoader } from "react-spinners";
 
 const SoftwareList = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -13,15 +16,18 @@ const SoftwareList = () => {
   console.log("hello: " + Maincategory + " " + Subcategory);
 
   const [SoftwareData, setSoftwareData] = useState([]);
+  const [loading, setLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     get
       .SoftwareAll()
       .then((data) => {
         setSoftwareData(data);
+        setLoading(false); // Set loading to false once the data is fetched
       })
       .catch((error) => {
         showToast("Hiba történt az adatok lekérése közben", "error");
+        setLoading(false); // Set loading to false in case of an error
       });
   }, []);
 
@@ -114,67 +120,76 @@ const SoftwareList = () => {
         <h1 className="text-2xl text-black font-semibold mb-8 mt-2 ml-12 hover-scale-element:hover hover-scale-element">
           Szoftverlista
         </h1>
-        <ul>
-          {filteredSoftwareData.map((software) => (
-            <li
-              key={software.softwareID}
-              className="pb-8 px-4 hover-scale-element:hover hover-scale-element FadeInSmall"
-            >
-              <div className="bg-white rounded-25 pt-12 pb-12 pr-12 border border-gray-400 flex">
-                {/* Container for the image (1/3 of the width) */}
-                <div className="w-1/3 flex justify-center items-center">
-                  <Link
-                    to={`/szoftverek/${transliterate(
-                      software.category.categoryGroup.name
-                    )}/${transliterate(software.category.name)}/${transliterate(
-                      software.name
-                    )}`}
-                    className="flex items-center"
-                  >
-                    <img
-                      src={software.logo_link}
-                      alt="Software Placeholder"
-                      className="pl-10 pr-10"
-                      draggable="false"
-                      style={{ width: "auto", height: "auto" }}
-                    />
-                  </Link>
-                </div>
 
-                {/* Container for the data (2/3 of the width) */}
-                <div className="w-2/3">
-                  <Link
-                    to={`/szoftverek/${transliterate(
-                      software.category.categoryGroup.name
-                    )}/${transliterate(software.category.name)}/${transliterate(
-                      software.name
-                    )}`}
-                    className="text-3xl font-semibold text-black"
-                  >
-                    {software.name}
-                  </Link>
-                  <Link to={`/cegek/${transliterate(software.company.name)}`}>
-                    <p className="text-gray-600 text-xl mb-2 mt-2">
-                      {software.company.name}
-                    </p>
-                  </Link>
-                  <div className="flex items-center">
-                    <span className="text-black text-lg mr-2">
-                      Vélemények:
-                      <StarIcon
-                        fontSize="medium"
-                        className="starmargin"
-                        style={{ color: "rgb(255, 210, 48)" }}
-                      />
-                    </span>
+        {loading ? ( // Render loading spinner if loading is true  // NEED TO FURTHER ENHANCE IT, maybe add text, color, different animations, size
+          <div className="flex justify-center items-center mt-40">
+            <ClipLoader color={"#B5B4B4"} loading={loading} size={250} /> 
+          </div>
+        ) : (
+          <>
+            <ul>
+              {filteredSoftwareData.map((software) => (
+                <li
+                  key={software.softwareID}
+                  className="pb-8 px-4 hover-scale-element:hover hover-scale-element FadeInSmall"
+                >
+                  <div className="bg-white rounded-25 pt-12 pb-12 pr-12 border border-gray-400 flex">
+                    {/* Container for the image (1/3 of the width) */}
+                    <div className="w-1/3 flex justify-center items-center">
+                      <Link
+                        to={`/szoftverek/${transliterate(
+                          software.category.categoryGroup.name
+                        )}/${transliterate(software.category.name)}/${transliterate(
+                          software.name
+                        )}`}
+                        className="flex items-center"
+                      >
+                        <img
+                          src={software.company.logo_link}
+                          alt="Software Placeholder"
+                          className="pl-10 pr-10"
+                          draggable="false"
+                          style={{ width: "auto", height: "auto" }}
+                        />
+                      </Link>
+                    </div>
+
+                    {/* Container for the data (2/3 of the width) */}
+                    <div className="w-2/3">
+                      <Link
+                        to={`/szoftverek/${transliterate(
+                          software.category.categoryGroup.name
+                        )}/${transliterate(software.category.name)}/${transliterate(
+                          software.name
+                        )}`}
+                        className="text-3xl font-semibold text-black"
+                      >
+                        {software.name}
+                      </Link>
+                      <Link to={`/cegek/${transliterate(software.company.name)}`}>
+                        <p className="text-gray-600 text-xl mb-2 mt-2">
+                          {software.company.name}
+                        </p>
+                      </Link>
+                      <div className="flex items-center">
+                        <span className="text-black text-lg mr-2">
+                          Vélemények:
+                          <StarIcon
+                            fontSize="medium"
+                            className="starmargin"
+                            style={{ color: "rgb(255, 210, 48)" }}
+                          />
+                        </span>
+                      </div>
+                      <p className="text-gray-700">{software.description}</p>
+                    </div>
                   </div>
-                  <p className="text-gray-700">{software.description}</p>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-        {noResultsMessage}
+                </li>
+              ))}
+            </ul>
+            {noResultsMessage}
+          </>
+        )}
       </div>
     </div>
   );
