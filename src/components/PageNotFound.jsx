924783b4d5e37bd7e21f6cb912954a72bd3c007b
Gondocs Robert <gondocs.robert@gmail.com>
2023-10-06 14:11:16 +0200

    added svg to PageNotFound, removed unnecessary footers

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../css/PageNotFound.css";
+import NotFoundSvg from "./assets/NotFoundSvg";
 
 const NotFound = () => {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="bg-white py-8 px-20 rounded-full shadow-lg mb-24 FadeInSmall">
-        <h1 className="text-4xl font-semibold text-gray-800 mb-4 mt-4">
+      <div className="bg-white py-8 px-20 rounded-40 shadow-lg mb-24 FadeInSmall">
+        <NotFoundSvg />
+        <h1 className="text-4xl font-semibold text-gray-800 mb-4 mt-4 text-center">
           404 - Az oldal nem található
         </h1>
-        <p className="text-gray-600 mt-2 mb-4 text-xl">
+        <p className="text-gray-600 mt-2 mb-4 text-xl text-center">
           Az általad keresett oldal nem létezik.
         </p>
         <div className="items-center justify-center flex-col flex">
--- src/index.js
+++ src/index.js
@@ -1,50 +1,135 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import {Navbar} from './components/Navbar/Navbar';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import LoginPage from './components/LoginAndRegister/loginPage';
-import Register from './components/LoginAndRegister/RegisterPage';
-import PageNotFound from './components/PageNotFound';
-import {HomePage} from './components/MainPage';
-import { Footer } from './components/Footer';
-import SoftwareList from './components/SoftwareList';
-import SoftwareDetail from './components/SoftwareDetail';
-import { Apitest } from './components/api/apitest';
-import { Toaster } from 'react-hot-toast';
-import { CompanyList } from './components/CompanyList';
-import CompanyDetail from './components/CompanyDetail';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./css/index.css";
+import { Navbar } from "./components/Navbar/Navbar";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/LoginAndRegister/loginPage";
+import Register from "./components/LoginAndRegister/RegisterPage";
+import PageNotFound from "./components/PageNotFound";
+import { HomePage } from "./components/MainPage";
+import { Footer } from "./components/Footer";
+import SoftwareList from "./components/SoftwareList";
+import SoftwareDetail from "./components/SoftwareDetail";
+import { Apitest } from "./components/api/apitest";
+import { Toaster } from "react-hot-toast";
+import { CompanyList } from "./components/CompanyList";
+import CompanyDetail from "./components/CompanyDetail";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
   <React.StrictMode>
-    <Router> 
-      <Toaster/>
-      <Navbar/>
-        <Routes> 
-            <Route path='/' element={ <> <HomePage/> <Footer/> </> }> </Route>
-            <Route path='/belepes' element={ <> <LoginPage/> <Footer/> </> }>  </Route>
-            <Route path='/regisztracio' element={ <> <Register/> <Footer/> </> }>  </Route>
-            <Route path='*' element={ <> <PageNotFound/> <Footer/> </> }> </Route>
-            <Route path='/szoftverek' element={ <> <SoftwareList/> <Footer/> </> } > </Route>
-            <Route path="/szoftverek/:Maincategory" element= { <> <SoftwareList/> <Footer/> </> } />
-            <Route path="/szoftverek/:Maincategory/:Subcategory" element= { <> <SoftwareList/> <Footer/> </> } />
-            <Route path="/szoftverek/:Maincategory/:SubCategory/:name" element= { <> <SoftwareDetail/> <Footer/> </> } />
-            <Route path="/cegek/:name" element= { <> <CompanyDetail/> <Footer/> </> } />
+    <Router>
+      <Toaster />
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              {" "}
+              <HomePage /> <Footer />{" "}
+            </>
+          }
+        >
+          {" "}
+        </Route>
+        <Route
+          path="/belepes"
+          element={
+            <>
+              {" "}
+              <LoginPage />{" "}
+            </>
+          }
+        >
+          {" "}
+        </Route>
+        <Route
+          path="/regisztracio"
+          element={
+            <>
+              {" "}
+              <Register />{" "}
+            </>
+          }
+        >
+          {" "}
+        </Route>
+        <Route
+          path="*"
+          element={
+            <>
+              {" "}
+              <PageNotFound />{" "}
+            </>
+          }
+        >
+          {" "}
+        </Route>
+        <Route
+          path="/szoftverek"
+          element={
+            <>
+              {" "}
+              <SoftwareList /> <Footer />{" "}
+            </>
+          }
+        >
+          {" "}
+        </Route>
+        <Route
+          path="/szoftverek/:Maincategory"
+          element={
+            <>
+              {" "}
+              <SoftwareList /> <Footer />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/szoftverek/:Maincategory/:Subcategory"
+          element={
+            <>
+              {" "}
+              <SoftwareList /> <Footer />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/szoftverek/:Maincategory/:SubCategory/:name"
+          element={
+            <>
+              {" "}
+              <SoftwareDetail /> <Footer />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/cegek/:name"
+          element={
+            <>
+              {" "}
+              <CompanyDetail /> <Footer />{" "}
+            </>
+          }
+        />
 
-            <Route path="/cegek/" element= { <> <CompanyList/> <Footer/> </> } />
+        <Route
+          path="/cegek/"
+          element={
+            <>
+              {" "}
+              <CompanyList /> <Footer />{" "}
+            </>
+          }
+        />
 
-
-            <Route path='/test' element= { <Apitest/> }></Route>
-
-        </Routes>
+        <Route path="/test" element={<Apitest />}></Route>
+      </Routes>
     </Router>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
