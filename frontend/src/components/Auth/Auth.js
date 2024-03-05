aef2d024ab281875e70bd38ce286be4e6c7fb141
Róbert Göndöcs <gondocs.robert@gmail.com>
2024-03-05 23:18:19 +0100

    discard httpOnly token

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -16,7 +16,7 @@ const AuthProvider = ({ children }) => {
 
     const setToken = (newToken) => {
         setToken_(newToken);
-        Cookies.set('token', newToken, { httpOnly: true }); // Use Cookies.set
+        Cookies.set('token', newToken); // Use Cookies.set
     };
 
     const setUserData = (newUserData) => {
@@ -37,10 +37,10 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         if (token) {
             axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
-            Cookies.set('token', token, { httpOnly: true });
+            Cookies.set('token', token);
         } else {
             delete axios.defaults.headers.common['Authorization'];
-            Cookies.remove('token', { httpOnly: true });
+            Cookies.remove('token');
         }
     }, [token]);
 
