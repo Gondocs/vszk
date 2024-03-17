75ca7d94841ebc3fef78582ee58847b0c5e663f7
Róbert Göndöcs <gondocs.robert@gmail.com>
2024-03-17 17:49:36 +0100

    add UserByIdDTO

---
+++ backend/Models/DTO/UserByIdDTO.cs
@@ -0,0 +1,18 @@
+
+namespace vszk.Models.DTO
+{
+    public class UserByIdDTO
+    {
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Role { get; set; }
+
+        public string Country { get; set; }
+
+        public string Settlement { get; set; }
+    }
+}
