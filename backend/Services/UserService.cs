ad390c9e43d2a236dd0428f2075aea116a7c0f69
Róbert Göndöcs <gondocs.robert@gmail.com>
2024-03-21 00:29:36 +0100

    add UpdateUserSettlement to UserService

--- backend/Services/UserService.cs
+++ backend/Services/UserService.cs
@@ -18,5 +18,21 @@ namespace vszk.Services
         {
             return await _context.User.FindAsync(id);
         }
+
+        public async Task<User> UpdateUserSettlement(UserSettlementDTO userSettlementDTO)
+        {
+            var user = await _context.User.FindAsync(userSettlementDTO.UserID);
+            if (user == null)
+            {
+                return null;
+            }
+
+            user.Country = userSettlementDTO.Country;
+            user.Settlement = userSettlementDTO.Settlement;
+
+            _context.User.Update(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
     }
 }
