4b8818ef659402205d1f13e530608768720603a3
Róbert Göndöcs <gondocs.robert@gmail.com>
2024-03-21 00:36:40 +0100

    Seperate Searchbar into a different file

---
+++ frontend/src/components/Navbar/Searchbar/SearchBar.jsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { get } from "../../api/api";
+import { showToast } from "../../toasts/toast";
+import { transliterate } from "../../api/transliteration";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import NotFoundSvg from "../../assets/NotFoundSvg";
+import { ClipLoader } from "react-spinners";
+import NewMenu from "../DropdownMenus/SoftwareCategoriesDropdown";
+import { useAuth } from "../../Auth/Auth";
+import ProfileDropdown from "../DropdownMenus/ProfileDropdown";
+import { useMediaQuery } from "react-responsive";
+import { slide as Menu } from "react-burger-menu";
+import "../../../css/sidebar.css";
+
+const SearchBar = ({
+    searchQuery,
+    handleInputChange,
+    handleSearchFocus,
+    handleSearchBlur,
+    handleSearchEnter,
+    handleSearchButtonClick,
+    isSearchFocused,
+    loading,
+    hasSearchResults,
+    filteredSoftwareData,
+    handleLinkClick,
+    parent,
+    token,
+  }) => (
+    <>
+      <div
+        className={`flex-grow relative px-5`}
+      >
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Keresés"
+            className="pl-5 pr-16 pt-2 pb-2 rounded-lg bg-gray-700 text-white focus:outline-none w-full"
+            value={searchQuery}
+            onChange={handleInputChange}
+            onFocus={handleSearchFocus}
+            onBlur={handleSearchBlur}
+            onKeyDown={handleSearchEnter}
+          />
+          <button
+            onClick={handleSearchButtonClick}
+            className="absolute top-0 right-0 h-full bg-gray-700 text-white rounded-r-lg px-6 hover:bg-gray-600 focus:outline-none border-l-2 border-gray-800"
+            style={{ top: 0 }}
+          >
+            Keresés
+          </button>
+        </div>
+  
+        {isSearchFocused && searchQuery.length >= 1 && (
+          <div
+            className="absolute mt-2 z-99 bg-white rounded-lg shadow-md max-h-96 overflow-y-auto p-4"
+            style={{
+              width: "85%",
+              maxHeight: "32rem",
+            }}
+            ref={parent}
+          >
+            {loading ? (
+              <div className="flex justify-center items-center">
+                <ClipLoader color={"#B5B4B4"} loading={loading} size={50} />
+              </div>
+            ) : hasSearchResults ? (
+              filteredSoftwareData.map((software) => (
+                <Link
+                  key={software.softwareID}
+                  to={`/szoftverek/${transliterate(
+                    software.category_group
+                  )}/${transliterate(software.category)}/${transliterate(
+                    software.name
+                  )}`}
+                  className="flex items-center px-4 py-2 hover:bg-gray-200 text-gray-800 hover:text-black hover:rounded-lg"
+                  onClick={handleLinkClick}
+                  style={{ height: "130px" }}
+                >
+                  <div className="w-1/3">
+                    <div className="flex items-center justify-center">
+                      <img
+                        src={software.logo_link}
+                        alt={software.name}
+                        className=""
+                        style={{
+                          width: "auto",
+                          height: "auto",
+                          maxHeight: "80px",
+                          paddingLeft: "30%",
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className="w-2/3 text-2xl font-semibold"
+                    style={{ paddingLeft: "20%" }}
+                  >
+                    {software.name}
+                    <br />
+                    <div className="text-base mt-1">{software.category}</div>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <div className="text-black text-center text-2xl">
+                Nem található szoftver.
+                <div
+                  className="mt-6 items-center"
+                  style={{
+                    width: "70%",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                  }}
+                >
+                  <NotFoundSvg />
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </>
+  );
+
+export default SearchBar;
