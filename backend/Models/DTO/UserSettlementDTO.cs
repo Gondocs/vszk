16a720d4040eda0df4677ec13d57be96f9a22eea
Róbert Göndöcs <gondocs.robert@gmail.com>
2024-03-21 00:28:16 +0100

    create UpdateUserSettlementDTO

---
+++ backend/Models/DTO/UserSettlementDTO.cs
@@ -0,0 +1,10 @@
+namespace vszk.Models.DTO
+{
+    public class UserSettlementDTO
+    {   
+        public int UserID { get; set; }
+        public string Country { get; set; }
+        
+        public string Settlement { get; set; }
+    }
+}
