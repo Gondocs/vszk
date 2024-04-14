368495f733550e3a05a4a0018b62a6b805b76801
Róbert Göndöcs <gondocs.robert@gmail.com>
2024-04-14 23:23:12 +0200

    add role check to ProtectedRoute

--- frontend/src/components/Routes/ProtectedRoute.jsx
+++ frontend/src/components/Routes/ProtectedRoute.jsx
@@ -1,8 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../Auth/Auth";
+import InsufficentPermissions from "../PageNotFound/InsufficentPermissions";
+import { Navbar } from "../Navbar/Navbar";
 
-export const ProtectedRoute = () => {
-  const { token } = useAuth();
+export const ProtectedRoute = ({ rolesAllowed, ...props }) => {
+  const { token, role } = useAuth();
 
   // Check if the user is authenticated
   if (!token) {
@@ -10,6 +12,12 @@ export const ProtectedRoute = () => {
     return <Navigate to="/belepes" />;
   }
 
-  // If authenticated, render the child routes
-  return <Outlet />;
+  // Check if the user's role allows access to the route
+  if (!rolesAllowed.includes(role)) {
+    // If role not allowed, redirect to a page indicating insufficient permissions
+    return <> <Navbar/> <InsufficentPermissions/> </>;
+  }
+
+  // If authenticated and role allowed, render the child routes
+  return <Outlet {...props} />;
 };
