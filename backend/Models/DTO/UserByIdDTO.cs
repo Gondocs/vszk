9ce43eb444d52baf1904cd2fc4bd5ba715be0d33
Róbert Göndöcs <gondocs.robert@gmail.com>
2024-04-16 21:31:53 +0200

    Remove role from userByIdDTO

--- backend/Models/DTO/UserByIdDTO.cs
+++ backend/Models/DTO/UserByIdDTO.cs
@@ -3,13 +3,12 @@ namespace vszk.Models.DTO
 {
     public class UserByIdDTO
     {
+        public int UserID { get; set; }
         public string Email { get; set; }
 
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
-
-        public string Role { get; set; }
 
         public string Country { get; set; }
 
