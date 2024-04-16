47c97c3bbfd986c05bb70315796eaa0e5ca74983
Róbert Göndöcs <gondocs.robert@gmail.com>
2024-04-16 21:32:09 +0200

    create UserDataChangeDTO #6

---
+++ backend/Models/DTO/UserDataChangeDTO.cs
@@ -0,0 +1,12 @@
+namespace vszk.Models.DTO
+{
+    public class UserDataChangeDTO
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email  { get; set; }
+        public string Country { get; set; }
+        public string Settlement { get; set; }
+    }
+}
